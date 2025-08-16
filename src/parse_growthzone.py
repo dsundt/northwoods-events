b11fefad60d294f1231673097777af7b2319e69a
dsundt <dsundt@gmail.com>
2025-08-16 00:27:44 -0500

    Update parse_growthzone.py

--- src/parse_growthzone.py
+++ src/parse_growthzone.py
@@ -1,25 +1,45 @@
 from bs4 import BeautifulSoup
 
-def parse(html):
+def parse(html: str):
     """
-    GrowthZone/ChamberMaster list/search pages
-    Returns list of dicts: {title, url, date_text, venue_text, iso_datetime?}
+    GrowthZone / ChamberMaster list/search pages.
+    Return list of dicts: title, url, date_text, venue_text, iso_datetime?
     """
     soup = BeautifulSoup(html, "lxml")
     items = []
-    for el in soup.select(".mn-content .listing, .cm-body .listing, .mn-list .listing, .listing"):
-        a = el.select_one('a[href*="/events/"]')
+
+    # Common listing blocks
+    candidates = soup.select(
+        ".mn-content .listing, .cm-body .listing, .mn-list .listing, .listing, "
+        ".mn-event, .mn-event-card, li.event, div.event"
+    )
+
+    for el in candidates:
+        a = el.select_one('a[href*="/events/"]') or el.find("a", href=True)
         if not a:
             continue
+
         title = (a.get_text() or "").strip()
-        url = a.get("href", "").strip()
+        url = (a.get("href") or "").strip()
 
-        # date blocks vary
-        dt_el = el.select_one(".date, .mn-date, .gz-date, .cm-date, time[datetime]")
-        iso_dt = dt_el.get("datetime").strip() if dt_el and dt_el.name == "time" and dt_el.get("datetime") else None
-        date_text = (dt_el.get_text() if dt_el else "").strip()
+        dt_el = el.select_one(".date, .mn-date, .gz-date, .cm-date, time[datetime], .event-date")
+        if dt_el and dt_el.name == "time" and dt_el.get("datetime"):
+            iso_dt = dt_el.get("datetime").strip()
+            date_text = dt_el.get_text().strip()
+        else:
+            iso_dt = None
+            date_text = (dt_el.get_text() if dt_el else "").strip()
 
-        venue_el = el.select_one(".location, .mn-location, .cm-location, .gz-location")
+        venue_el = el.select_one(".location, .mn-location, .cm-location, .gz-location, .event-location")
         venue_text = (venue_el.get_text() if venue_el else "").strip()
-        items.append({"title": title, "url": url, "date_text": date_text, "venue_text": venue_text, "iso_datetime": iso_dt})
+
+        items.append({
+            "title": title,
+            "url": url,
+            "date_text": date_text,
+            "venue_text": venue_text,
+            "iso_datetime": iso_dt,
+            "iso_end": None,
+        })
+
     return items
